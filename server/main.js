26b1411401a55392f68fc588a455c8732b33ffa0
Juan Camilo Bages <jcbages@outlook.com>
2017-10-13 11:56:00 -0500

    final

--- server/main.js
+++ server/main.js
@@ -5,4 +5,8 @@ import '../imports/startup/FacebookLogin.jsx';
 
 Meteor.startup(() => {
   // code to run on server at startup
+	connectHandler.use(function (req, res, next) {
+		res.setHeader('Strict-Transport-Security', 'max-age=2592000; includeSubDomains'); // 2592000s / 30 days
+		return next();
+	});
 });
